5419311122bcd08ec9cabb68c284451ff58af822
panyi <chuileyiyefeng1s@gmail.com>
2023-11-10 18:07:31 +0800

    application增加全局灰度

--- app/src/main/java/com/example/rico/MyApplication.java
+++ app/src/main/java/com/example/rico/MyApplication.java
@@ -1,6 +1,15 @@
 package com.example.rico;
 
+import android.app.Activity;
 import android.app.Application;
+import android.graphics.ColorMatrix;
+import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.facebook.drawee.backends.pipeline.Fresco;
 
@@ -12,5 +21,52 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         Fresco.initialize(this);
+
+        //以下设置全局黑白
+//        Paint mPaint = new Paint();
+//        ColorMatrix mColorMatrix = new ColorMatrix();
+//        mColorMatrix.setSaturation(0);
+//        mPaint.setColorFilter(new ColorMatrixColorFilter(mColorMatrix));
+//
+//        registerActivityLifecycleCallbacks(new ActivityLifecycleCallbacks() {
+//            @Override
+//            public void onActivityCreated(@NonNull Activity activity, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+//                View decorView = activity.getWindow().getDecorView();
+//                decorView.setLayerType(View.LAYER_TYPE_HARDWARE, mPaint);
+//            }
+//
+//            @Override
+//            public void onActivityStarted(@NonNull Activity activity) {
+//
+//            }
+//
+//            @Override
+//            public void onActivityResumed(@NonNull Activity activity) {
+//
+//            }
+//
+//            @Override
+//            public void onActivityPaused(@NonNull Activity activity) {
+//
+//            }
+//
+//            @Override
+//            public void onActivityStopped(@NonNull Activity activity) {
+//
+//            }
+//
+//            @Override
+//            public void onActivitySaveInstanceState(@NonNull Activity activity, @NonNull Bundle outState) {
+//
+//            }
+//
+//            @Override
+//            public void onActivityDestroyed(@NonNull Activity activity) {
+//
+//            }
+//        });
+
+
     }
+
 }
